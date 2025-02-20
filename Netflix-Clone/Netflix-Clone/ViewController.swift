332f58ee555d4b46e8e0e0000d8118f436fde717
DerenB <dbozerprogramming@gmail.com>
2025-02-20 17:26:01 -0500

    Project Creation

---
+++ Netflix-Clone/Netflix-Clone/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Netflix-Clone
+//
+//  Created by Deren Bozer on 2/20/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
