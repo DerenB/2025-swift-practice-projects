37edc3e702a9f553ce6edfe3aa22aa4e45973e01
DerenB <dbozerprogramming@gmail.com>
2025-02-23 20:06:35 -0500

    Content

--- SwiftUI-Weather/SwiftUI-Weather/ContentView.swift
+++ SwiftUI-Weather/SwiftUI-Weather/ContentView.swift
@@ -15,7 +15,7 @@ struct ContentView: View {
             
             /// Background Gradient
             LinearGradient(
-                gradient: Gradient(colors: [.blue, .white]),
+                gradient: Gradient(colors: [.blue, Color("LightBlue")]),
                 startPoint: .topLeading,
                 endPoint: .bottomTrailing
             )
@@ -23,7 +23,68 @@ struct ContentView: View {
             
             /// Vertical Content Stack
             VStack {
+                
+                /// City Text
                 Text("Cupertine, CA")
+                    .font(.system(size: 32, weight: .medium, design: .default))
+                    .foregroundStyle(Color.white)
+                    .padding()
+                
+                /// Weather Icon & Text Stack
+                VStack(spacing: 8) {
+                    
+                    /// Weather Icon
+                    Image(systemName: "cloud.sun.fill")
+                        .renderingMode(.original)
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .frame(width: 180, height: 180)
+                    
+                    /// Temperature Text
+                    Text("76°")
+                        .font(.system(size: 70, weight: .medium))
+                        .foregroundStyle(Color.white)
+                    
+                }
+                .padding(.bottom, 40)
+                
+                
+                
+                /// Daily Temp Stack
+                HStack(spacing: 20) {
+                    WeatherDayView(
+                        dayOfWeek: "TUE",
+                        imageName: "cloud.sun.fill",
+                        temperature: 74
+                    )
+                    
+                    WeatherDayView(
+                        dayOfWeek: "WED",
+                        imageName: "sun.max.fill",
+                        temperature: 88
+                    )
+                    
+                    WeatherDayView(
+                        dayOfWeek: "THU",
+                        imageName: "wind.snow",
+                        temperature: 55
+                    )
+                    
+                    WeatherDayView(
+                        dayOfWeek: "FRI",
+                        imageName: "sunset.fill",
+                        temperature: 60
+                    )
+                    
+                    WeatherDayView(
+                        dayOfWeek: "SAT",
+                        imageName: "snow",
+                        temperature: 25
+                    )
+                }
+                
+                /// Spacer
+                Spacer()
             }
         }
     }
@@ -32,3 +93,38 @@ struct ContentView: View {
 #Preview {
     ContentView()
 }
+
+
+struct WeatherDayView: View {
+    
+    var dayOfWeek: String
+    var imageName: String
+    var temperature: Int
+    
+    var body: some View {
+        VStack {
+            /// Day Text
+            Text(dayOfWeek)
+                .font(.system(size: 16, weight: .medium, design: .default))
+                .foregroundStyle(Color.white)
+            
+            /// Day Weather
+            Image(systemName: imageName)
+                .renderingMode(.original)
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .frame(width: 40, height: 40)
+            
+            /// Temperature Text
+            Text("\(temperature)°")
+                .font(.system(size: 28, weight: .medium))
+                .foregroundStyle(Color.white)
+        }
+    }
+}
+
+
+
+
+
+
