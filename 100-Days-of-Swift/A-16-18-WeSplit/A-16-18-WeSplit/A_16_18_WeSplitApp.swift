5fbee0a9e3cd28e1c7c9ac3435e2f2d4f9ee32dd
DerenB <dbozerprogramming@gmail.com>
2025-05-27 19:17:00 -0400

    Project Creation

---
+++ 100-Days-of-Swift/A-16-18-WeSplit/A-16-18-WeSplit/A_16_18_WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  A_16_18_WeSplitApp.swift
+//  A-16-18-WeSplit
+//
+//  Created by Deren Bozer on 5/27/25.
+//
+
+import SwiftUI
+
+@main
+struct A_16_18_WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ 100-Days-of-Swift/A-16-18-WeSplit/A-16-18-WeSplit/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  A-16-18-WeSplit
+//
+//  Created by Deren Bozer on 5/27/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
