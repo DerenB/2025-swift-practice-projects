ad0bb42857031bac6dce496c13de661bfc567cfb
DerenB <dbozerprogramming@gmail.com>
2025-06-07 15:47:30 -0400

    Days 16-18 WeSplit

--- 100-Days-of-Swift/WeSplit-16-18/WeSplit-16-18/ContentView.swift
+++ 100-Days-of-Swift/WeSplit-16-18/WeSplit-16-18/ContentView.swift
@@ -8,8 +8,114 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @FocusState private var amountIsFocused: Bool
+    
+    @State private var checkAmount = 0.0
+    @State private var numberOfPeople = 2
+    @State private var tipPercentage = 20
+    
+    let tipOptions = [0, 10, 15, 20, 25]
+    
+    var billTotal: Double {
+        let peopleCount = Double(numberOfPeople + 2)
+        let tipSelection = Double(tipPercentage)
+        
+        let tipValue = (checkAmount / 100) * tipSelection
+        let grandTotal = checkAmount + tipValue
+        
+        return grandTotal
+    }
+    
+    var totalPerPerson: Double {
+        let peopleCount = Double(numberOfPeople + 2)
+        let tipSelection = Double(tipPercentage)
+        
+        let tipValue = (checkAmount / 100) * tipSelection
+        let grandTotal = checkAmount + tipValue
+        let amountPerPerson = grandTotal / peopleCount
+        
+        return amountPerPerson
+    }
+    
     var body: some View {
-        Text("Hello")
+        
+        NavigationStack {
+            Form {
+                
+                Section("User Input") {
+                    
+                    TextField(
+                        "Amount",
+                        value: $checkAmount,
+                        format: .currency(code: Locale.current.currency?.identifier ?? "USD")
+                    )
+                    .keyboardType(.decimalPad)
+                    .focused($amountIsFocused)
+                    
+                    Picker("Number of People", selection: $numberOfPeople) {
+                        ForEach(2..<100) {
+                            Text("\($0) people")
+                        }
+                    }
+                    
+                }
+                
+                
+                
+                Section(header: Text("Picker: Segmented")) {
+                    Picker("Tip Percentage", selection: $tipPercentage) {
+                        ForEach(tipOptions, id: \.self) {
+                            Text($0, format: .percent)
+                        }
+                    }
+                    .pickerStyle(.segmented)
+                }
+                
+                
+                
+                Section(header: Text("Total")) {
+                    Text(billTotal, format: .currency(code: Locale.current.currency?.identifier ?? "USD"))
+                }
+                
+                
+                
+                Section(header: Text("Amount per Person")) {
+                    Text(totalPerPerson, format: .currency(code: Locale.current.currency?.identifier ?? "USD"))
+                }
+
+                
+                
+                Section(header: Text("Picker: Drop List")) {
+                    Picker("Number of People", selection: $numberOfPeople) {
+                        ForEach(2..<100) {
+                            Text("\($0) people")
+                        }
+                    }
+                    .pickerStyle(.navigationLink)
+                }
+
+                
+                
+                Section(header: Text("Picker: New Page")) {
+                    Picker("Number of People", selection: $numberOfPeople) {
+                        ForEach(2..<100) {
+                            Text("\($0) people")
+                        }
+                    }
+                    .pickerStyle(.navigationLink)
+                }
+                
+            }
+            .navigationTitle("We Split")
+            .toolbar {
+                if amountIsFocused {
+                    Button("Done") {
+                        amountIsFocused = false
+                    }
+                }
+            }
+        }
     }
 }
 
