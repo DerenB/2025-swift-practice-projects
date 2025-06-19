3ddd72615c5eb145ddb8d86bd43037d493cca375
Deren B <47374311+DerenB@users.noreply.github.com>
2025-06-18 23:34:43 -0400

    Day 19 project creation

---
+++ 100-Days-of-Swift/Convert-19/Convert-19/Convert_19App.swift
@@ -0,0 +1,17 @@
+//
+//  Convert_19App.swift
+//  Convert-19
+//
+//  Created by Deren Bozer on 6/18/25.
+//
+
+import SwiftUI
+
+@main
+struct Convert_19App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
